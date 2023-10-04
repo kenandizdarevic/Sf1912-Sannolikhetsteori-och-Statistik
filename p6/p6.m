35e5f5ae13e64cff3fae1b9e1ecb7c59d4ea131c
Kenan Dizdarevic <kenandi@kth.se>
2023-10-04 16:28:10 +0200

    Done problem 6

---
+++ p6/p6.m
@@ -0,0 +1,39 @@
+data = load("data\moore.dat");
+
+trans_per_area = data(:, 1);
+year = data(:, 2);
+
+w = log(year);
+
+X = [ones(length(trans_per_area), 1), trans_per_area];
+
+[beta_hat, ~, ~, ~, stats] = regress(w, X);
+
+% Plottar residualerna.
+res = w - X*beta_hat;
+
+subplot(2,1,1), normplot(res)
+ylabel('Sannolikhet')
+title('Normalitetsplott')
+xlabel('Teoretiska kvantiler')
+
+subplot(2,1,2), hist(res)
+title('Histogram över resiudaler')
+ylim([0, 65]);
+
+% Beräkna R^2
+mean_w = mean(w);
+
+% Beräkna SST
+SST = sum((w - mean(w)).^2);
+
+% SSR finns i stats(1)
+R_squared = 1 - (stats(1) / SST); 
+fprintf('R-kvadrat: %.4f\n', R_squared);
+
+% Förutsägelse år 2025
+year_2025 = 2021;
+X_2025 = [1, year_2025];
+log_prediction_2025 = X_2025 * beta_hat;
+prediction_2025 = exp(log_prediction_2025);
+fprintf('Förutsedd mängd transistorer år 2025: %.2f\n', prediction_2025);
